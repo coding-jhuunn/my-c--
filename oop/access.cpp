5154a0b1ad8c1de75e9bf8102108f1096e75488d
coding-jhuunn <codingatjhunn@gmail.com>
2025-03-07 15:56:42 +0800

    add private and public class

---
+++ oop/access.cpp
@@ -0,0 +1,8 @@
+class Demo {
+    private:
+        int secret = 123;  // Private Member (Cannot be accessed outside)
+    
+    public:
+        int data = 10;  // Public Member (Accessible)
+    };
+    
